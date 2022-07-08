ff676e5861c458dc8e719d76a4a930a7f4a8892b
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-07-08 17:34:12 +0000

    changed solution

---
+++ TwistedSum.js
@@ -0,0 +1,19 @@
+function twistedSum(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i.toString().length > 1) {
+      i.toString()
+        .split("")
+        .forEach((el) => {
+          sum += parseInt(el);
+        });
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+//Example Test
+//# N = 10
+console.log(twistedSum(10)); //---> Result = 46 (1+2+3+4+5+6+7+8+9+(1+0))
